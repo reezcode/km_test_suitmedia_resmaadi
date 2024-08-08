8aabcd1c4f96b475b1366616e1b7564519eddfb7
dinug-c1 <resmaadi@gmail.com>
2024-08-08 20:08:04 +0700

    feat: additional

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'core/di/service_locator.dart';
 
 // KAMPUS MERDEKA TEST SUITMEDIA - MOBILE DEVELOPER
 // RESMA ADI NUGROHO - UNIVERSITAS DIPONEGORO
+// 20:07 WIB 08 AGUSTUS 2024
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
